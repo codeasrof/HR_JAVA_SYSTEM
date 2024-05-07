4b5df44745abbd4bf060a0f570374f0829ebe0d4
codeasrof <rar2023@icloud.com>
2024-05-07 08:23:39 -0300

    init

---
+++ src/main/java/com/HRApp/HRApp/DataConfiguration.java
@@ -0,0 +1,19 @@
+package com.HRApp.HRApp;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class DataConfiguration {
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jbdc.Driver");
+        dataSource.setUrl("jbdc:mysql");
+        return dataSource;
+    }
+}
