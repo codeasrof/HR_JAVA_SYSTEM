727ffba123be4e68e68d4d8d2c516a014fbb9cec
codeasrof <rar2023@icloud.com>
2024-05-07 08:49:15 -0300

    adding DataSource and JPA Hibernate.

--- src/main/java/com/HRApp/HRApp/DataConfiguration.java
+++ src/main/java/com/HRApp/HRApp/DataConfiguration.java
@@ -5,6 +5,9 @@ import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @Configuration
 public class DataConfiguration {
@@ -13,7 +16,19 @@ public class DataConfiguration {
     public DataSource dataSource(){
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jbdc.Driver");
-        dataSource.setUrl("jbdc:mysql");
+        dataSource.setUrl("jbdc:mysql://localhost:3306/RHApp?userTimezone=true&serverTimezone=UTC");
+        dataSource.setUsername("root");
+        dataSource.setPassword("GodIsGood@2023#");
         return dataSource;
+    }
+
+    public JpaVendorAdapter jpaVendorAdapter(){
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.MYSQL);
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(true);
+        adapter.setDatabasePlatform("org.hibernate.dialect.MariaDBDialect");
+        adapter.setPrepareConnection(true);
+        return adapter;
     }
 }
